817b4325bfa3a0e21367da0390c1cf3fdbdc295f
Alexander Jacks <alexanderthejacks@gmail.com>
2019-12-11 16:16:19 -0800

    :wrench: enable PWA features, new brand icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Top100Crypto`,
+    description: `Sort week's top 100 cryptocurrency by trading volume and value.`,
+    author: `@top100crypto`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `top100crypto`,
+        short_name: `top100crypto`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#b22222`,
+        theme_color: `#b22222`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/dollar-wings.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
